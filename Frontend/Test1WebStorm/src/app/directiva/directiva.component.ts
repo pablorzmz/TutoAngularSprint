a818af203e80e8fd56c09f92b1579cf565d7579f
Pablo <jose.ramirez16@ucr.ac.cr>
2019-07-15 19:35:44 -0600

    Agrego llamado método y directiva ngif

--- Frontend/Test1WebStorm/src/app/directiva/directiva.component.ts
+++ Frontend/Test1WebStorm/src/app/directiva/directiva.component.ts
@@ -7,5 +7,9 @@ import { Component, OnInit } from '@angular/core';
 export class DirectivaComponent  {
 
   listaCurso: string[] = ['TypeScript', 'JavaScript', 'Java SE', 'C#', 'PHP'];
+  habilitar: boolean = true;
   constructor() {}
+  setHabilitar(): void {
+    this.habilitar = this.habilitar?false:true;
+  }
 }
