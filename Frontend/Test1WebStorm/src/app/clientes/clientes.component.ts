59896cc3e23b7d610c916dcedaf5b2c14f2b566b
Pablo <jose.ramirez16@ucr.ac.cr>
2019-07-15 21:29:39 -0600

    Rutas y bootstrap ahora local

--- Frontend/Test1WebStorm/src/app/clientes/clientes.component.ts
+++ Frontend/Test1WebStorm/src/app/clientes/clientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Cliente} from './cliente';
-import {CLIENTES} from './clientes.json';
+import {ClienteService} from './cliente.service';
+
 
 @Component({
   selector: 'app-clientes',
@@ -9,10 +10,13 @@ import {CLIENTES} from './clientes.json';
 export class ClientesComponent implements OnInit {
 
   clientes: Cliente[];
-  constructor() { }
+  constructor( private clienteService: ClienteService) {
+  }
 
   ngOnInit() {
-    this.clientes = CLIENTES;
+    this.clienteService.getClientes().subscribe(
+      clientes => this.clientes = clientes
+    );
   }
 
 }
