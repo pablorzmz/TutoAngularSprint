9f52b88344a96dbc63fa3cd025080c8d2731fbac
Pablo <jose.ramirez16@ucr.ac.cr>
2019-07-18 16:48:49 -0600

    Entity para clientes y modificaciones en el archivo prop para ver los comandos sql que se corren.

---
+++ Backend/src/main/java/com/bolsadeideas/springboot/backend/apirest/model/entity/Cliente.java
@@ -0,0 +1,63 @@
+package com.bolsadeideas.springboot.backend.apirest.model.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String email;
+
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    private Date createdAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    private static final long serialVersionUID = -2577117508487543501L;
+}
